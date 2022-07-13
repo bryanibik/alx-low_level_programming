f0245d1034d59e1ff33eeaead7998371355c3511
bryanibik <bryanibik9@gmail.com>
2022-07-12 17:06:23 -0700

    hh

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,21 @@
+#include "main.h"
+#include <string.h>
+
 /**
- * _strcpy -copy a string
- * @dest: Destination value
- * @src: Source value
- *
- * Return: the pointer to dest
+ * puts_half -> puts half
+ * @str: string param
  */
 
-char *_strcpy(char *dest, char *src)
+void puts_half(char *str)
 {
-	int a, b = 0;
+	int x, y, i;
 
-	for (a = 0; src[a] != '\0'; ++a)
-	{
-		dest[b] = src[a];
-		++b;
-	}
-	dest[b] = '\0';
-
-	return (dest);
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
